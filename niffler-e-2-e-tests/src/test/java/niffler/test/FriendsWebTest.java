ed2127759f7ee7f21e6643d8f3d0b9703ac22389
stanislav <stanislavkovalev.m@gmail.com>
2023-04-25 23:24:33 +0300

    Добавление возможности указания нескольких параметров с аннотацией User

--- niffler-e-2-e-tests/src/test/java/niffler/test/FriendsWebTest.java
+++ niffler-e-2-e-tests/src/test/java/niffler/test/FriendsWebTest.java
@@ -1,14 +1,7 @@
 package niffler.test;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static niffler.jupiter.annotation.User.UserType.INVITATION_SENT;
-import static niffler.jupiter.annotation.User.UserType.WITH_FRIENDS;
-
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
-import io.qameta.allure.Allure;
 import io.qameta.allure.AllureId;
 import niffler.jupiter.annotation.User;
 import niffler.jupiter.extension.UsersQueueExtension;
@@ -16,34 +9,68 @@ import niffler.model.UserJson;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
+import static niffler.jupiter.annotation.User.UserType.INVITATION_SENT;
+import static niffler.jupiter.annotation.User.UserType.WITH_FRIENDS;
+
 @ExtendWith(UsersQueueExtension.class)
 public class FriendsWebTest extends BaseWebTest {
 
-  @AllureId("102")
-  @Test
-  void friendsShouldBeVisible0(@User(userType = WITH_FRIENDS) UserJson user) {
-    Allure.step("open page", () -> Selenide.open("http://127.0.0.1:3000/main"));
-    $("a[href*='redirect']").click();
-    $("input[name='username']").setValue(user.getUsername());
-    $("input[name='password']").setValue(user.getPassword());
-    $("button[type='submit']").click();
+    @AllureId("102")
+    @Test
+    void friendsShouldBeVisible0(@User(userType = WITH_FRIENDS) UserJson user, @User(userType = WITH_FRIENDS) UserJson user2) {
+        step("open page", () -> Selenide.open("http://127.0.0.1:3000/main"));
+        step("Проверка списка друзей под " + user.getUsername(), () -> {
+            $("a[href*='redirect']").click();
+            $("input[name='username']").setValue(user.getUsername());
+            $("input[name='password']").setValue(user.getPassword());
+            $("button[type='submit']").click();
 
-    $("a[href*='friends']").click();
-    $$(".table tbody tr").shouldHave(sizeGreaterThan(0));
-  }
+            $("a[href*='friends']").click();
+            $$(".table tbody tr").shouldHave(sizeGreaterThan(0));
+            $(".header__logout").click();
+        });
+        step("Проверка списка друзей под " + user2.getUsername(), () -> {
+            $("a[href*='redirect']").shouldBe(Condition.visible, Duration.ofSeconds(5)).click();
+            $("input[name='username']").setValue(user2.getUsername());
+            $("input[name='password']").setValue(user2.getPassword());
+            $("button[type='submit']").click();
 
-  @AllureId("103")
-  @Test
-  void friendsShouldBeVisible1(@User(userType = INVITATION_SENT) UserJson user) {
-    Allure.step("open page", () -> Selenide.open("http://127.0.0.1:3000/main"));
-    $("a[href*='redirect']").click();
-    $("input[name='username']").setValue(user.getUsername());
-    $("input[name='password']").setValue(user.getPassword());
-    $("button[type='submit']").click();
+            $("a[href*='friends']").click();
+            $$(".table tbody tr").shouldHave(sizeGreaterThan(0));
+            $(".header__logout").click();
+        });
+    }
 
-    $("a[href*='people']").click();
-    $$(".table tbody tr").find(Condition.text("Pending invitation"))
-        .should(Condition.visible);
-  }
+    @AllureId("103")
+    @Test
+    void friendsShouldBeVisible1(@User(userType = WITH_FRIENDS) UserJson user, @User(userType = INVITATION_SENT) UserJson user2) {
+        step("open page", () -> Selenide.open("http://127.0.0.1:3000/main"));
+        step("Проверка списка друзей под " + user.getUsername(), () -> {
+            $("a[href*='redirect']").click();
+            $("input[name='username']").setValue(user.getUsername());
+            $("input[name='password']").setValue(user.getPassword());
+            $("button[type='submit']").click();
+
+            $("a[href*='friends']").click();
+            $$(".table tbody tr").shouldHave(sizeGreaterThan(0));
+            $(".header__logout").click();
+        });
+        step("Проверка отправленного запроса в друзья у пользователя " + user.getUsername(), () -> {
+            $("a[href*='redirect']").click();
+            $("input[name='username']").setValue(user2.getUsername());
+            $("input[name='password']").setValue(user2.getPassword());
+            $("button[type='submit']").click();
+
+            $("a[href*='people']").click();
+            $$(".table tbody tr").find(Condition.text("Pending invitation"))
+                    .should(Condition.visible);
+        });
+    }
 
 }
