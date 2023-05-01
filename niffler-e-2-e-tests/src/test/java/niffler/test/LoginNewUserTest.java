50549faaaec69cee342f1b338ec5e9abb07e045b
stanislav <stanislavkovalev.m@gmail.com>
2023-05-01 21:32:22 +0300

    1) Создан экстеншен GenerateUserExtension для создания нового пользователя перед тестом. 2) Доработан NifflerUsersDAO и NifflerUsersDAOJdbc, добавлены методы получения, обновления и удаления пользователя. 3) В метод создания пользователя добавлена работа с транзакциями

--- niffler-e-2-e-tests/src/test/java/niffler/test/LoginNewUserTest.java
+++ niffler-e-2-e-tests/src/test/java/niffler/test/LoginNewUserTest.java
@@ -1,62 +1,51 @@
 package niffler.test;
 
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Allure;
+import niffler.db.dao.NifflerUsersDAO;
+import niffler.db.dao.NifflerUsersDAOJdbc;
+import niffler.db.entity.UserEntity;
+import niffler.jupiter.extension.GenerateUserExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.sql.SQLException;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
-import com.codeborne.selenide.Selenide;
-import io.qameta.allure.Allure;
-import io.qameta.allure.AllureId;
-import java.io.IOException;
-import java.util.Arrays;
-import niffler.db.dao.NifflerUsersDAO;
-import niffler.db.dao.NifflerUsersDAOJdbc;
-import niffler.db.entity.Authority;
-import niffler.db.entity.AuthorityEntity;
-import niffler.db.entity.UserEntity;
-import niffler.jupiter.annotation.ClasspathUser;
-import niffler.model.UserJson;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 public class LoginNewUserTest extends BaseWebTest {
 
-  private NifflerUsersDAO usersDAO = new NifflerUsersDAOJdbc();
-  private UserEntity ue;
+    @Test
+    @ExtendWith(GenerateUserExtension.class)
+    void loginTest(UserEntity ue) {
+        Allure.step("open page", () -> Selenide.open("http://127.0.0.1:3000/main"));
+        $("a[href*='redirect']").click();
+        $("input[name='username']").setValue(ue.getUsername());
+        $("input[name='password']").setValue(ue.getPassword());
+        $("button[type='submit']").click();
 
-  @BeforeEach
-  void createUserForTest() {
-    ue = new UserEntity();
-    ue.setUsername("valentin0");
-    ue.setPassword("12345");
-    ue.setEnabled(true);
-    ue.setAccountNonExpired(true);
-    ue.setAccountNonLocked(true);
-    ue.setCredentialsNonExpired(true);
-    ue.setAuthorities(Arrays.stream(Authority.values()).map(
-        a -> {
-          AuthorityEntity ae = new AuthorityEntity();
-          ae.setAuthority(a);
-          return ae;
-        }
-    ).toList());
-    usersDAO.createUser(ue);
-  }
+        $("a[href*='friends']").click();
+        $(".header").should(visible).shouldHave(text("Niffler. The coin keeper."));
+    }
 
-  @Test
-  void loginTest() throws IOException {
-    Allure.step("open page", () -> Selenide.open("http://127.0.0.1:3000/main"));
-    $("a[href*='redirect']").click();
-    $("input[name='username']").setValue(ue.getUsername());
-    $("input[name='password']").setValue(ue.getPassword());
-    $("button[type='submit']").click();
+    @Test
+    @ExtendWith(GenerateUserExtension.class)
+    void userLifecycleTest(UserEntity ue) throws SQLException {
+        UserEntity user;
 
-    $("a[href*='friends']").click();
-    $(".header").should(visible).shouldHave(text("Niffler. The coin keeper."));
-  }
-
+        final NifflerUsersDAO usersDAO = new NifflerUsersDAOJdbc();
+        String userId = usersDAO.getUserId(ue.getUsername());
+        UserEntity originalUser = usersDAO.getUser(userId);
+        System.out.println(originalUser.toString());
+        user = originalUser;
+        user.setUsername("amanda.dietrich2");
+        user.setPassword("12345");
+        user.setAccountNonExpired(false);
+        user.setEnabled(false);
+        usersDAO.updateUser(user);
+        System.out.println(usersDAO.getUser(userId).toString());
+        usersDAO.deleteUser(userId);
+    }
 }
